5f67c815a45a650c5ba75b7e942ee8c009c038f8
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-07-25 16:34:05 +0300

    Arrays review questions

---
+++ KotlinBasics/src/methods/ArrayReviewQuestions.kt
@@ -0,0 +1,44 @@
+package methods
+
+fun main() {
+
+    val factorialArray = Array(10) { 0 }
+    for (i in 0 until factorialArray.size) {
+        factorialArray[i] = calculateFactorialInArray(i)
+        println(factorialArray[i])
+    }
+
+    var numbers = Array(5){0}
+
+    for (index in 0 until numbers.size){
+        println("Enter number : ")
+        var number = readLine()!!.toInt()
+        numbers[index] = number
+    }
+
+    for (number in numbers){
+        print(number)
+    }
+
+
+}
+
+fun calculateFactorialInArray(number: Int): Int {
+
+    var counter = 1
+    var factorial = 1
+
+    if (number == 1) {
+        return 1
+    }
+
+    while (counter <= number) {
+        factorial *= counter
+        counter++
+    }
+
+    return factorial
+
+}
+
+
