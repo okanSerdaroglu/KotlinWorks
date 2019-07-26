964954c4cf36c0460f5726b8a6ce8a9107272a36
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-07-26 14:59:13 +0300

    arrays operations

---
+++ KotlinBasics/src/arrays/ArrayOperations.kt
@@ -0,0 +1,58 @@
+package arrays
+
+import java.util.*
+
+fun main() {
+
+    //copy Arrays
+    val arrayOne = arrayOf(5,1,3,90,4)
+    val arrayTwo = Array(5){0}
+
+    /*for (i in 0 until arrayOne.size){
+        arrayTwo [i] = arrayOne [i]
+    }*/
+
+    println(System.arraycopy(arrayOne,0,arrayTwo,0,arrayTwo.size))
+
+    // sorting arrays
+
+    for (number in arrayTwo){
+        println(number)
+    }
+
+    println()
+    Arrays.sort(arrayTwo)
+
+    for (number in arrayTwo){
+        println(number)
+    }
+
+    // compare Arrays
+    println()
+    println(Arrays.equals(arrayOne,arrayTwo))
+
+    val names = arrayListOf("okan","adem","erdinç")
+
+    for (i in 0 until names.size){
+        println(names[i])
+    }
+
+    names.add("ömer")
+    names.add("barış")
+    names.add("enes")
+
+    println(names)
+
+    println(names[2])
+    println(names.size)
+    println(names.remove("okan")) // returns boolean
+    names.set(0,"osman")
+
+    println(names)
+
+
+
+
+
+
+}
