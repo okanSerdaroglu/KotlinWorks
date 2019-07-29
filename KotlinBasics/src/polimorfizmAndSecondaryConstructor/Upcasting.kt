e8194599ad19e7c92fb9930db6a9b0c2ed5ef626
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-07-29 10:15:03 +0300

    upcasting

---
+++ KotlinBasics/src/polimorfizmAndSecondaryConstructor/Upcasting.kt
@@ -0,0 +1,50 @@
+package polimorfizmAndSecondaryConstructor
+
+open class Soldier {
+
+    open fun sayHello() {
+        println("hello")
+    }
+
+}
+
+class Er : Soldier () {
+
+    override fun sayHello() {
+
+        println("Er said hello")
+
+    }
+
+}
+
+class YuzBasi : Soldier () {
+
+    override fun sayHello() {
+
+        println("Yuzbasi said hello")
+
+    }
+
+}
+
+fun main() {
+
+    val soldier = Soldier()
+    val er = Er ()
+    val yuzBasi = YuzBasi ()
+
+    hazirOl(soldier)
+    hazirOl(er)
+    hazirOl(yuzBasi)
+
+
+
+}
+
+fun hazirOl (soldier: Soldier) {
+
+    soldier.sayHello()
+
+
+}
