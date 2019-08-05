62ab5cc07363d03557a34ac59b0059f33e4f75ea
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-08-05 14:54:31 +0300

    mutable and immutable sets

---
+++ KotlinBasics/src/collections/Set.kt
@@ -0,0 +1,26 @@
+package collections
+
+// setOf, mutableSetOf
+
+fun main() {
+
+    val immutableSet = setOf("okan","osman","ali","okan") // dont list repeated values
+
+    for (element in immutableSet) {
+        println(element)
+    }
+
+    println("********")
+
+    val mutableSet = mutableSetOf<Int>()
+
+    mutableSet.add(9)
+    mutableSet.add(6)
+    mutableSet.add(1)
+    mutableSet.add(6) /* sets don't add repeated values */
+    mutableSet.add(9)
+
+    println(mutableSet.size)
+
+
+}
